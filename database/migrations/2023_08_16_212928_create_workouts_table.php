95b525b1847109787835a09d4abc3477b0e80ce0
= <=>
2023-08-17 01:25:29 +0300

    creating database migrartions and model relationship

---
+++ database/migrations/2023_08_16_212928_create_workouts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('duration')->nullable();
+            $table->foreingId('coach_id')->consrtained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workouts');
+    }
+};
